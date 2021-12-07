0c13dad0d2195b5f24b10f21e0967d05b074d949
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2021-12-06 19:44:55 -0500

    importacion del autehnticacionCOmponente en application.ts

--- src/application.ts
+++ src/application.ts
@@ -1,11 +1,12 @@
+import {AuthenticationComponent} from '@loopback/authentication';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
@@ -40,5 +41,7 @@ export class TrabajoacademicoApp extends BootMixin(
         nested: true,
       },
     };
+
+    this.component(AuthenticationComponent);
   }
 }
